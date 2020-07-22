aa54477e608cddfe6f9b34062c952c461b662c79
dhruvs009 <dhruv.ds.009@gmail.com>
2020-07-22 22:47:59 +0530

    Home page setup - basic

--- lib/HomeState.dart
+++ lib/HomeState.dart
@@ -1,30 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:induction/Utilities.dart';
 import 'ColorCodes.dart';
 import 'Home.dart';
 
 class HomeState extends State<Home> {
+
   @override
   Widget build(BuildContext context) {
+    List<Widget> scheduleContents = [
+      Center(
+        child: Image.asset('assets/images/logofulldrop.png', width:Utilities.scale(MediaQuery.of(context).size.width-110,context), height: Utilities.vScale((MediaQuery.of(context).size.width-110),context)/2.41, color: MaterialColor(0xcccb2d6f, magentaColorCodes),),
+      ),
+    ];
     return Scaffold(
-      backgroundColor: MaterialColor(0xdd11292d, darkSeaGreenColorCodes),
-      body: SingleChildScrollView (
-          child: Container(
-            child: Column(
-              children: <Widget>[
-                Padding(padding: EdgeInsets.only(top: MediaQuery.of(context).padding.top*2)),
-                Text(
-                  "  Home",
-                  style: TextStyle(
-                      fontSize: 55,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                      fontFamily: "Poppins"
-                  ),
-                )
-              ],
-            ),
-          )
-      )
+        backgroundColor: MaterialColor(0xff262833, darkSeaGreenColorCodes),
+        body: SingleChildScrollView (
+            child: Container(
+              padding: EdgeInsets.only(top: Utilities.vScale(MediaQuery.of(context).padding.top*2, context), left: Utilities.scale(30,context), right: Utilities.scale(30,context), bottom: Utilities.vScale(30,context)),
+              child: Column(
+                  children: scheduleContents
+              ),
+            )
+        )
     );
   }
 }
